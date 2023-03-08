e1fe7ebec16c23e2603da2c9dfef87f644f2a386
Panatorn <panatorn@192.168.0.4>
2023-03-08 11:31:46 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+function CaLscore(){
+    let score = prompt("กรอกคะแนน")
+    if (score >= 80){
+        alert("คุณได้เกรด 4  ")
+    }
+    else if (score >= 75){
+        alert("คุณได้เกรด 3.5")
+    }
+    else if (score >= 70){
+        alert("คุณได้เกรด 3")
+    }
+    else if (score >= 65){
+        alert("คุณได้เกรด 2.5")
+    }
+    else if (score >= 60){
+        alert("คุณได้เกรด 2")
+    }
+    else if (score >= 55){
+        alert("คุณได้เกรด 1.5")
+    }
+    else if (score >= 50){
+        alert("คุณได้เกรด 1")
+    }
+    else 
+        alert("คุณได้เกรด 0 ติดต่อครูประจำชั้น")
+}
+    
+
+    
+
+    
+
+    
